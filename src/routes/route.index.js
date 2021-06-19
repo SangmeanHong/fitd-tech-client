6e866888154be60cd898bf653d7703b761dc0ca6
SangmeanHong <sangmean.hong@gmail.com>
2021-06-19 13:54:33 -0600

    added home page

--- src/routes/route.index.js
+++ src/routes/route.index.js
@@ -4,19 +4,21 @@ import { Container } from '@material-ui/core';
 import Auth from '../pages/Auth/auth.index';
 import ForgotPwd from '../pages/ForgotPwd/ForgotPwd.index';
 import ResetPwd from '../pages/ResetPwd/ResetPwd.index';
+import { Home } from '../pages/Home/Home.index';
 
 const RootRoute = () => {
-	return (
-		<BrowserRouter>
-			<Container>
-				<Switch>
-					<Route path='/auth' exact component={Auth} />
-					<Route path='/resetpw/:token/:email' exact component={ResetPwd} />
-					<Route path='/forgotPwd' exact component={ForgotPwd} />
-				</Switch>
-			</Container>
-		</BrowserRouter>
-	);
+    return (
+        <BrowserRouter>
+            <Container>
+                <Switch>
+                    <Route path="/" component={Home} />
+                    <Route path='/auth' exact component={Auth} />
+                    <Route path='/resetpw/:token/:email' exact component={ResetPwd} />
+                    <Route path='/forgotPwd' exact component={ForgotPwd} />
+                </Switch>
+            </Container>
+        </BrowserRouter>
+    );
 };
 
 export default RootRoute;
