9e4fbd225ef9c1e5385261332b1e956fabb94eb8
Felix <felixliu0122@gmail.com>
2021-06-19 22:02:51 -0600

    Added Navbar and Button components

--- src/routes/route.index.js
+++ src/routes/route.index.js
@@ -4,14 +4,14 @@ import { Container } from '@material-ui/core';
 import Auth from '../pages/Auth/auth.index';
 import ForgotPwd from '../pages/ForgotPwd/ForgotPwd.index';
 import ResetPwd from '../pages/ResetPwd/ResetPwd.index';
-import { Home } from '../pages/Home/Home.index';
+import Navbar from '../pages/Home/Navbar.index';
 
 const RootRoute = () => {
     return (
         <BrowserRouter>
             <Container>
                 <Switch>
-                    <Route path="/" component={Home} />
+                    <Route path="/" component={Navbar} />
                     <Route path='/auth' exact component={Auth} />
                     <Route path='/resetpw/:token/:email' exact component={ResetPwd} />
                     <Route path='/forgotPwd' exact component={ForgotPwd} />
