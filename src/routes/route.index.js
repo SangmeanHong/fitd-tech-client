0c5c9dad9c3bbac4647ef9e31e85b08b54083979
Felix <felixliu0122@gmail.com>
2021-06-20 23:38:33 -0600

    Added another image to the homepage. Changed some text. Fixed the border issue (thanks Nikk!).

--- src/routes/route.index.js
+++ src/routes/route.index.js
@@ -4,27 +4,25 @@ import { Container } from '@material-ui/core';
 import Auth from '../pages/Auth/auth.index';
 import ForgotPwd from '../pages/ForgotPwd/ForgotPwd.index';
 import ResetPwd from '../pages/ResetPwd/ResetPwd.index';
-
 import HomePage from '../pages/Home/HomePage.index';
 import Coaching from '../pages/Coaching/Coaching.index';
 import Content from '../pages/Content/Content.index';
-
 import Navbar from '../components/Navbar/Navbar';
 import Footer from '../components/Footer/Footer';
-
 
 const RootRoute = () => {
     return (
         <BrowserRouter>
-            <Container>
+            <Container disableGutters maxWidth={false}>
                 <Navbar />
                     <Switch>
-                        <Route path="/" component={HomePage} />
+                        
                         <Route path='/coaching' component={Coaching} />
                         <Route path='/content' component={Content} />
                         <Route path='/auth' exact component={Auth} />
                         <Route path='/resetpw/:token/:email' exact component={ResetPwd} />
                         <Route path='/forgotPwd' exact component={ForgotPwd} />
+                        <Route path="/" component={HomePage} />
                     </Switch>
                 <Footer />
             </Container>
