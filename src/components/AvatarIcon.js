5c80b2154823a048c06473dd2e812bb73c7accd3
SangmeanHong <sangmean.hong@gmail.com>
2021-07-06 20:04:32 -0600

    updated Avatar section

--- src/components/AvatarIcon.js
+++ src/components/AvatarIcon.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { AppBar, Typography, Toolbar, Avatar, Button } from '@material-ui/core';
 import useStyles from '../libs/AvatarStyle';
 import { Link, useHistory, useLocation } from 'react-router-dom';
@@ -8,57 +8,47 @@ import './Avatar.css';
 // import decode from 'jwt-decode';
 
 const AvatarIcon = () => {
-	const classes = useStyles();
-	const dispatch = useDispatch();
-	const history = useHistory();
-	const location = useLocation();
-	const [user, setUser] = useState(JSON.parse(localStorage.getItem('profile')));
+    const classes = useStyles();
+    const dispatch = useDispatch();
+    const history = useHistory();
+    const [user, setUser] = useState(JSON.parse(sessionStorage.getItem('profile')));
+    const logout = () => {
+        dispatch({ type: LOGOUT_REQUEST });
+        history.push('/');
+    };
 
-	const logout = () => {
-		dispatch({ type: LOGOUT_REQUEST });
-		history.push('/');
-		setUser(null);
-	};
+    useEffect(() => {
+        setUser(JSON.parse(sessionStorage.getItem('profile')));
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [sessionStorage.getItem('profile')]);
 
-	useEffect(() => {
-		// const token = user?.token;
-
-		// if (token) {
-		//     const decodedToken = decode(token);
-
-		//     if (decodedToken.exp * 1000 < new Date().getTime()) logout();
-		// }
-
-		setUser(JSON.parse(localStorage.getItem('profile')));
-	}, [location]);
-
-	return (
-		<Toolbar className={classes.toolbar}>
-			{user ? (
-				<div className={classes.profile}>
-					<Avatar
-						className={classes.purple}
-						alt={user.firstNname}
-						src={user.imageUrl}
-					>
-						{user.firstName.charAt(0)}
-					</Avatar>
-					<Button
-						variant='contained'
-						className={classes.logout}
-						color='secondary'
-						onClick={logout}
-					>
-						Logout
-					</Button>
-				</div>
-			) : (
-				<Link className='avatarButton' to='/auth'>
-					Get Started
-				</Link>
-			)}
-		</Toolbar>
-	);
+    return (
+        <Toolbar className={classes.toolbar}>
+            {user ? (
+                <div className={classes.profile}>
+                    <Avatar
+                        className={classes.purple}
+                        alt={user.firstNname}
+                        src={user.imageUrl}
+                    >
+                        {user.firstName.charAt(0)}
+                    </Avatar>
+                    <Button
+                        variant='contained'
+                        className={classes.logout}
+                        color='secondary'
+                        onClick={logout}
+                    >
+                        Logout
+                    </Button>
+                </div>
+            ) : (
+                <Link className='avatarButton' to='/auth'>
+                    Get Started
+                </Link>
+            )}
+        </Toolbar>
+    );
 };
 
 export default AvatarIcon;
--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -10,72 +10,71 @@ import AvatarIcon from '../AvatarIcon';
 import Dropdown from './Dropdown';
 
 const Navbar = () => {
-	const [click, setClick] = useState(false);
-	const [button, setButton] = useState(true);
-	const { authData, loading } = useSelector((state) => state.authReducer);
-	const handleClick = () => setClick(!click);
-	const closeMobileMenu = () => setClick(false);
-	const [dropdown, setDropdown] = useState(false);
+    const [click, setClick] = useState(false);
+    const [button, setButton] = useState(true);
+    const { authData, loading } = useSelector((state) => state.authReducer);
+    const handleClick = () => setClick(!click);
+    const closeMobileMenu = () => setClick(false);
+    const [dropdown, setDropdown] = useState(false);
+    const showButton = () => {
+        if (window.innerWidth <= 960) {
+            setButton(false);
+        } else {
+            setButton(true);
+        }
+    };
 
-	const showButton = () => {
-		if (window.innerWidth <= 960) {
-			setButton(false);
-		} else {
-			setButton(true);
-		}
-	};
+    const onMouseEnter = () => {
+        if (window.innerWidth < 960) {
+            setDropdown(false);
+        } else {
+            setDropdown(true);
+        }
+    };
 
-	const onMouseEnter = () => {
-		if (window.innerWidth < 960) {
-			setDropdown(false);
-		} else {
-			setDropdown(true);
-		}
-	};
+    const onMouseLeave = () => {
+        if (window.innerWidth < 960) {
+            setDropdown(false);
+        } else {
+            setDropdown(false);
+        }
+    };
 
-	const onMouseLeave = () => {
-		if (window.innerWidth < 960) {
-			setDropdown(false);
-		} else {
-			setDropdown(false);
-		}
-	};
+    useEffect(() => {
+        showButton();
+        window.addEventListener('resize', showButton);
+        return {
+            // window.removeEventListener('resize', showButton)
+        };
+    }, []);
 
-	useEffect(() => {
-		showButton();
-		window.addEventListener('resize', showButton);
-		return {
-			// window.removeEventListener('resize', showButton)
-		};
-	}, []);
+    useEffect(() => {
+        return {};
+    }, []);
 
-	useEffect(() => {
-		return {};
-	}, []);
-
-	return (
-		<>
-			<IconContext.Provider value={{ color: '#fff' }}>
-				<nav className='navbar'>
-					<div className='navbar-container container'>
-						<Link
-							to='/'
-							style={{ color: 'black', fontWeight: 'bolder' }}
-							className='navbar-logo'
-							onClick={closeMobileMenu}
-						>
-							<RiDoorOpenFill className='navbar-icon' />
-							FITD
-						</Link>
-						<div className='menu-icon' onClick={handleClick}>
-							{click ? (
-								<FaTimes style={{ color: 'black' }} />
-							) : (
-								<FaBars style={{ color: 'black' }} />
-							)}
-						</div>
-						<ul className={click ? 'nav-menu active' : 'nav-menu'}>
-							{/* <li className='nav-item'>
+    return (
+        <>
+            <IconContext.Provider value={{ color: '#fff' }}>
+                <nav className='navbar'>
+                    <div className='navbar-container container'>
+                        <Link
+                            to='/'
+                            style={{ color: 'black', fontWeight: 'bolder' }}
+                            className='navbar-logo'
+                            onClick={closeMobileMenu}
+                        >
+                            <RiDoorOpenFill className='navbar-icon' />
+                            FITD
+                        </Link>
+                        <div className='menu-icon' onClick={handleClick}>
+                            {click ? (
+                                <FaTimes style={{ color: 'black' }} />
+                            ) : (
+                                <FaBars style={{ color: 'black' }} />
+                            )}
+                        </div>
+                        <ul className={click ? 'nav-menu active' : 'nav-menu'}>
+                            {/* <li className='nav-item'>
                                 <Link
                                     to='/category'
                                     className='nav-links'
@@ -84,39 +83,39 @@ const Navbar = () => {
                                     Category
                                 </Link>
                             </li> */}
-							{/* <li className='nav-item'> */}
-							<li
-								className='nav-item'
-								onMouseEnter={onMouseEnter}
-								onMouseLeave={onMouseLeave}
-							>
-								<Link
-									to='/content'
-									className='nav-links'
-									onClick={closeMobileMenu}
-								>
-									Content <i className='fas fa-caret-down' />
-								</Link>
-								{dropdown && <Dropdown />}
-							</li>
-							<li className='nav-item'>
-								<Link
-									to='/coaching'
-									className='nav-links'
-									onClick={closeMobileMenu}
-								>
-									Coaching
-								</Link>
-							</li>
-							<li className='nav-btn'>
-								<AvatarIcon />
-							</li>
-						</ul>
-					</div>
-				</nav>
-			</IconContext.Provider>
-		</>
-	);
+                            {/* <li className='nav-item'> */}
+                            <li
+                                className='nav-item'
+                                onMouseEnter={onMouseEnter}
+                                onMouseLeave={onMouseLeave}
+                            >
+                                <Link
+                                    to='/content'
+                                    className='nav-links'
+                                    onClick={closeMobileMenu}
+                                >
+                                    Content <i className='fas fa-caret-down' />
+                                </Link>
+                                {dropdown && <Dropdown />}
+                            </li>
+                            <li className='nav-item'>
+                                <Link
+                                    to='/coaching'
+                                    className='nav-links'
+                                    onClick={closeMobileMenu}
+                                >
+                                    Coaching
+                                </Link>
+                            </li>
+                            <li className='nav-btn'>
+                                <AvatarIcon />
+                            </li>
+                        </ul>
+                    </div>
+                </nav>
+            </IconContext.Provider>
+        </>
+    );
 };
 
 export default Navbar;
