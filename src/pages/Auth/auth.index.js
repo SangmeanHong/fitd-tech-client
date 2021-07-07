66ee29f2c4574150c2dda231c18b0df5e1a494f0
SangmeanHong <sangmean.hong@gmail.com>
2021-07-06 20:35:32 -0600

    protected routes

--- src/pages/Auth/auth.index.js
+++ src/pages/Auth/auth.index.js
@@ -32,7 +32,7 @@ const SignUp = () => {
     const classes = useStyles();
     const dispatch = useDispatch();
     const history = useHistory();
-
+    const userInfo = JSON.parse(sessionStorage.getItem('profile'));
     const [isSignup, setIsSignup] = useState(false);
     const [formData, setFormData] = useState(initialState);
     const [showPassword, setShowPassword] = useState(false);
@@ -56,10 +56,7 @@ const SignUp = () => {
                 dispatch(actionSignUp(formData));
             }
         } else {
-            dispatch(actionSignIn(formData));
-        }
-        if (!authData) {
-            history.push('/');
+            dispatch(actionSignIn(formData, history));
         }
     };
 
@@ -105,107 +102,114 @@ const SignUp = () => {
     }, [dispatch, isSignup]);
 
     return (
-        <Container component='main' maxWidth='xs'>
-            <Paper className={classes.paper} elevation={3}>
-                <Avatar className={classes.avatar}>
-                    <LockOutlinedIcon />
-                </Avatar>
-                <Typography variant='h5'>{isSignup ? 'Sign Up' : 'Sign In'}</Typography>
-                {loading && <LoadingSpinner />}
-                {err && <MessageBox severity='error'>{err}</MessageBox>}
-                <form className={classes.form} onSubmit={handleSubmit}>
-                    <Grid container spacing={2}>
-                        {isSignup && (
-                            <Fragment>
-                                <Input
-                                    name='firstName'
-                                    label='First Name'
-                                    handleChange={handleChange}
-                                    autoFocus
-                                    half
-                                    type='text'
-                                />
-                                <Input
-                                    name='lastName'
-                                    label='Last Name'
-                                    handleChange={handleChange}
-                                    half
-                                    type='text'
-                                />
-                            </Fragment>
-                        )}
-                        <Input
-                            name='email'
-                            label='Email Address'
-                            handleChange={handleChange}
-                            type='email'
-                        />
-                        <Input
-                            name='password'
-                            label='Password'
-                            handleChange={handleChange}
-                            type={showPassword ? 'text' : 'password'}
-                            handleShowPassword={handleShowPassword}
-                        />
-                        {isSignup && (
-                            <Input
-                                name='confirmPassword'
-                                label='confirm Password'
-                                handleChange={handleChange}
-                                type='password'
-                            />
-                        )}
-                    </Grid>
-                    <Button
-                        type='submit'
-                        fullWidth
-                        variant='contained'
-                        color='primary'
-                        className={classes.submit}
-                        disabled={isSignup && !isValid}
-                    >
-                        {isSignup ? 'Sign Up' : 'Sign In'}
-                    </Button>
-                    <Grid item>
-                        {!isSignup && (
-                            <GoogleLogin
-                                clientId={process.env.REACT_APP_CLIENT_ID}
-                                render={(renderProps) => (
-                                    <Button
-                                        className={classes.googleButton}
-                                        color='primary'
-                                        fullWidth
-                                        onClick={renderProps.onClick}
-                                        disabled={renderProps.disabled}
-                                        startIcon={<AuthIcon />}
-                                        variant='contained'
-                                    >
-                                        Google Sign In
-                                    </Button>
-                                )}
-                                onSuccess={googleSuccess}
-                                onFailure={googleError}
-                                cookiePolicy='single_host_origin'
-                            />
-                        )}
-                    </Grid>
-                    <Grid container justify='flex-end'>
-                        <Grid item>
-                            <Button onClick={switchMode}>
-                                {isSignup
-                                    ? 'Already have an account? Sign In'
-                                    : "Don't have an account? Sign Up"}
-                            </Button>
-                        </Grid>
-                        <Grid item>
-                            {!isSignup && (
-                                <Button onClick={forgotPassword}>Forgot your password?</Button>
-                            )}
-                        </Grid>
-                    </Grid>
-                </form>
-            </Paper>
-        </Container>
+        <>
+            {
+                !userInfo ?
+                    <Container component='main' maxWidth='xs'>
+                        <Paper className={classes.paper} elevation={3}>
+                            <Avatar className={classes.avatar}>
+                                <LockOutlinedIcon />
+                            </Avatar>
+                            <Typography variant='h5'>{isSignup ? 'Sign Up' : 'Sign In'}</Typography>
+                            {loading && <LoadingSpinner />}
+                            {err && <MessageBox severity='error'>{err}</MessageBox>}
+                            <form className={classes.form} onSubmit={handleSubmit}>
+                                <Grid container spacing={2}>
+                                    {isSignup && (
+                                        <Fragment>
+                                            <Input
+                                                name='firstName'
+                                                label='First Name'
+                                                handleChange={handleChange}
+                                                autoFocus
+                                                half
+                                                type='text'
+                                            />
+                                            <Input
+                                                name='lastName'
+                                                label='Last Name'
+                                                handleChange={handleChange}
+                                                half
+                                                type='text'
+                                            />
+                                        </Fragment>
+                                    )}
+                                    <Input
+                                        name='email'
+                                        label='Email Address'
+                                        handleChange={handleChange}
+                                        type='email'
+                                    />
+                                    <Input
+                                        name='password'
+                                        label='Password'
+                                        handleChange={handleChange}
+                                        type={showPassword ? 'text' : 'password'}
+                                        handleShowPassword={handleShowPassword}
+                                    />
+                                    {isSignup && (
+                                        <Input
+                                            name='confirmPassword'
+                                            label='confirm Password'
+                                            handleChange={handleChange}
+                                            type='password'
+                                        />
+                                    )}
+                                </Grid>
+                                <Button
+                                    type='submit'
+                                    fullWidth
+                                    variant='contained'
+                                    color='primary'
+                                    className={classes.submit}
+                                    disabled={isSignup && !isValid}
+                                >
+                                    {isSignup ? 'Sign Up' : 'Sign In'}
+                                </Button>
+                                <Grid item>
+                                    {!isSignup && (
+                                        <GoogleLogin
+                                            clientId={process.env.REACT_APP_CLIENT_ID}
+                                            render={(renderProps) => (
+                                                <Button
+                                                    className={classes.googleButton}
+                                                    color='primary'
+                                                    fullWidth
+                                                    onClick={renderProps.onClick}
+                                                    disabled={renderProps.disabled}
+                                                    startIcon={<AuthIcon />}
+                                                    variant='contained'
+                                                >
+                                                    Google Sign In
+                                                </Button>
+                                            )}
+                                            onSuccess={googleSuccess}
+                                            onFailure={googleError}
+                                            cookiePolicy='single_host_origin'
+                                        />
+                                    )}
+                                </Grid>
+                                <Grid container justify='flex-end'>
+                                    <Grid item>
+                                        <Button onClick={switchMode}>
+                                            {isSignup
+                                                ? 'Already have an account? Sign In'
+                                                : "Don't have an account? Sign Up"}
+                                        </Button>
+                                    </Grid>
+                                    <Grid item>
+                                        {!isSignup && (
+                                            <Button onClick={forgotPassword}>Forgot your password?</Button>
+                                        )}
+                                    </Grid>
+                                </Grid>
+                            </form>
+                        </Paper>
+                    </Container>
+                    :
+                    history.push('/')
+            }
+        </>
     );
 };
 
--- src/routes/route.index.js
+++ src/routes/route.index.js
@@ -1,10 +1,10 @@
 import React from 'react';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { BrowserRouter, Route, Switch, useHistory } from 'react-router-dom';
 import { Container } from '@material-ui/core';
 import Auth from '../pages/Auth/auth.index';
 import ForgotPwd from '../pages/ForgotPwd/ForgotPwd.index';
 import ResetPwd from '../pages/ResetPwd/ResetPwd.index';
-import HomePage from '../pages/Home/HomePage.index';
+import Home from '../pages/Home/HomePage.index';
 import Coaching from '../pages/Coaching/Coaching.index';
 import Content from '../pages/Content/Content.index';
 import Navbar from '../components/Navbar/Navbar';
@@ -15,6 +15,9 @@ import Articles from '../pages/Articles/Articles.index';
 import { CoachDetail } from '../pages/CoachDetailPage/CoachDetail';
 
 const RootRoute = () => {
+    console.log('라우트 동작');
+    const userInfo = JSON.parse(sessionStorage.getItem('profile'));
+    console.log(`userInfo`, userInfo)
     return (
         <BrowserRouter>
             <Container disableGutters maxWidth={false}>
@@ -28,8 +31,8 @@ const RootRoute = () => {
                     <Route path='/resetpw/:token/:email' exact component={ResetPwd} />
                     <Route path='/forgotPwd' exact component={ForgotPwd} />
                     <Route path='/coaches/:name' exact component={CoachDetail} />
-                    <Route path='/' component={HomePage} exact />
-                    <ErrorPage />
+                    <Route path='/' component={Home} exact />
+                    <Route render={() => <ErrorPage />} />
                 </Switch>
                 <Footer />
             </Container>
