d80d7f43df74f7fe08d4f925328851c911d1b9cb
Mitchell-Sung <mitchell.sung@gmail.com>
2021-07-21 01:38:15 -0600

    Implement  bookScheduler for displaying calendar

--- src/components/BookScheduler/BookScheduler.js
+++ src/components/BookScheduler/BookScheduler.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Calendar, momentLocalizer } from 'react-big-calendar';
 import moment from 'moment';
 import 'react-big-calendar/lib/css/react-big-calendar.css';
@@ -8,7 +8,13 @@ import 'react-big-calendar/lib/css/react-big-calendar.css';
 
 import './styles.css';
 
+import { useDispatch } from 'react-redux';
+import actionAddContent from '../../redux/actions/actionAddContent';
+import axios from 'axios';
+
 const BookScheduler = () => {
+	const dispatch = useDispatch();
+
 	const [events, setEvents] = useState([]);
 
 	// provide localization with the momentLocalizer function.
@@ -34,6 +40,60 @@ const BookScheduler = () => {
 		alert(`title: ${title}, start: ${startTime}, end: ${endTime}
 		`);
 	};
+
+	useEffect(() => {
+		const getContent = async () => {
+			const result = await axios.get('http://localhost:8080/api/content');
+			console.log(`result.data`, result.data);
+			result.data.map((data) => {
+				if (data.events.length > 0) {
+					console.log(`data.events`, data.events);
+					const preDate = data.events[0];
+					console.log(`preDate`, preDate);
+					const startDateObj = new Date(preDate.start);
+					const hours = startDateObj.getUTCHours();
+					const minutes = startDateObj.getUTCMinutes();
+					const second = startDateObj.getUTCSeconds();
+					const year = startDateObj.getFullYear();
+					const month = startDateObj.getMonth();
+					const date = startDateObj.getDate();
+
+					const endDateObj = new Date(preDate.end);
+					const endhours = endDateObj.getUTCHours();
+					const endminutes = endDateObj.getUTCMinutes();
+					const endsecond = endDateObj.getUTCSeconds();
+					const endyear = endDateObj.getFullYear();
+					const endmonth = endDateObj.getMonth();
+					const enddate = endDateObj.getDate();
+
+					preDate.start = new Date(year, month, date, hours, minutes);
+					preDate.end = new Date(
+						endyear,
+						endmonth,
+						enddate,
+						endhours,
+						endminutes
+					);
+					setEvents(data.events);
+				}
+			});
+		};
+		getContent();
+	}, []);
+
+	// const addContentObj = {
+	// 	title: 'title',
+	// 	description: 'desc',
+	// 	contentPrice: 30,
+	// 	duration: 40,
+	// 	via: 'video',
+	// 	type: 'group',
+	// 	detail: 'details',
+	// 	fileUrl: 'uploadFile',
+	// 	events,
+	// };
+	// console.log(`addContentObj`, addContentObj);
+	// dispatch(actionAddContent(addContentObj));
 
 	return (
 		<div className='container-main'>
