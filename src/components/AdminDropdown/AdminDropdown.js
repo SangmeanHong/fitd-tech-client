8c55b17ef77193e58724975dbe126784286094ae
SangmeanHong <sangmean.hong@gmail.com>
2021-07-31 22:15:49 -0600

    changed css on navbar

--- src/components/AdminDropdown/AdminDropdown.js
+++ src/components/AdminDropdown/AdminDropdown.js
@@ -3,10 +3,10 @@ import { DropdownButton, Dropdown } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 import './AdminDropdown.css'
 
-export const AdminDropdown = () => {
+export const AdminDropdown = ({ scrollDown }) => {
     return (
-        <div className="adminDropdown">
-            <DropdownButton id="dropdown" title="Admin">
+        <div className={`adminDropdown`}>
+            <DropdownButton id="dropdown" className={scrollDown ? 'scrollDownAdminDropDown' : 'defaultAdminDropDown'} title="Admin">
                 <Link to="/manageusers">Manage User</Link>
                 <Link to="/managecontents">Manage Content</Link>
                 <Link to="/action-3">Something else</Link>
