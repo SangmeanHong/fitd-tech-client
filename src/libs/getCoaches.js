93abaa1ca519592859774b56f94ee92c578dfe66
SangmeanHong <sangmean.hong@gmail.com>
2021-08-03 00:12:39 -0600

    define console to check API

--- src/libs/getCoaches.js
+++ src/libs/getCoaches.js
@@ -3,6 +3,7 @@ import { API } from '../config';
 
 export const getCoaches = async (search) => {
     let coaches;
+    console.log(`API check....`, API)
     if (search) {
         const { data } = await axios.get(
             `${API}/api/search/coach/${search}`,
